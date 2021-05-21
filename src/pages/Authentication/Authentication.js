85cf052284694ff50e67cd4b4a1b959c0cec4529
alexstotsky <alexander.stotsky@code-care.pro>
2021-05-21 14:43:05 +0300

    Fix missed props, add defaults, lint fixes

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -2,23 +2,38 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import HFIcon from '../../ui/HFIcon'
+import { version } from '../../../package.json'
+
 import AuthenticationInitForm from './AuthenticationInitForm'
 import AuthenticationUnlockForm from './AuthenticationUnlockForm'
 import AuthenticationConnectingForm from './AuthenticationConnectingForm'
-import { version } from '../../../package.json'
 
 import './style.css'
 
 export default class Authentication extends React.PureComponent {
-  static propTypes ={
+  static propTypes = {
+    configured: PropTypes.bool,
+    wsConnected: PropTypes.bool,
+    isPaperTrading: PropTypes.bool,
     onInit: PropTypes.func.isRequired,
     onReset: PropTypes.func.isRequired,
     onUnlock: PropTypes.func.isRequired,
   }
 
+  static defaultProps = {
+    configured: false,
+    wsConnected: false,
+    isPaperTrading: false,
+  }
+
   render() {
     const {
-      wsConnected, configured, onUnlock, onInit, onReset, isPaperTrading,
+      onInit,
+      onReset,
+      onUnlock,
+      configured,
+      wsConnected,
+      isPaperTrading,
     } = this.props
 
     return (
